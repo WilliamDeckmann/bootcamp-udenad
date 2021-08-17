af98729ead794c813c57ba8473dcfae2bec86faa
WilliamDeckmann <u773560@rts-365.dk>
2021-08-17 14:33:08 +0200

    Add assignment 01b

--- eks_01b_DOMogEvents/js/index.js
+++ eks_01b_DOMogEvents/js/index.js
@@ -3,4 +3,60 @@ document.addEventListener("DOMContentLoaded", () => {
 
 	// skriv din kode her...
 
+	// Elements & Count
+	let TextCountElement = document.querySelector("#text-count");
+	let CountIncreaseButton = document.querySelector("#btn-count-increase");
+	let CountDecreaseButton = document.querySelector("#btn-count-decrease");
+	let Count = 5;
+	let MaxCount = 15;
+	let MinCount = 0;
+
+	// Display Count
+	DisplayTextCount();
+	console.log(`Current count: ${Count}`);
+
+	// Display text count
+	function DisplayTextCount() {
+	
+		TextCountElement.textContent=`Count: ${Count}`;
+	};
+
+	// Increase text count
+	CountIncreaseButton.addEventListener("click", () => {
+
+		// If under maximum count
+		if(Count < MaxCount) {
+			Count++;
+			DisplayTextCount();
+			console.log("Count increased by 1");
+			console.log(`Current count: ${Count}`);
+		}else{
+			console.log(`Can't go over count of ${MaxCount}`);
+		};
+
+		// If reached maximum count
+		if(Count == MaxCount) {
+			console.log("Reached maximum count!");
+		};
+	});
+
+	// Decrease text count
+	CountDecreaseButton.addEventListener("click", () => {
+
+		// If over minimum count
+		if(Count > MinCount) {
+			Count--;
+			DisplayTextCount();
+			console.log("Count decreased by 1");
+			console.log(`Current count: ${Count}`);
+		}else{
+			console.log(`Can't go under count of ${MinCount}`);
+		};
+
+		// If reached minimum count
+		if(Count == MinCount) {
+			console.log("Reached minimum count!");
+		};
+	});
+
 }); // Afslutter: DOMContentLoaded
