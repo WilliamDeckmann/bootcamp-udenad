44266d1eaf9543bb27b10ddb21f21d78ee6b53ce
WilliamDeckmann <u773560@rts-365.dk>
2021-08-19 15:07:06 +0200

    Add assignment 03a

--- eks_03a_arrays/js/index.js
+++ eks_03a_arrays/js/index.js
@@ -3,4 +3,18 @@ document.addEventListener("DOMContentLoaded", () => {
 
 	// skriv din kode her...
 
+	let CharacterArray = ["Anders", "Fedtmul", "Micky"]
+
+	// Version: 1
+	for(let i = 0; i < CharacterArray.length; i++) {
+		let CharacterItem = document.createElement("li");
+		CharacterItem.textContent = CharacterArray[i];
+		document.querySelector("#liste").appendChild(CharacterItem);
+	};
+
+	// Version: 2
+	CharacterArray.forEach((Character) => {
+		document.querySelector("#liste").innerHTML += `<li> ${Character} </li>`;
+	});
+
 }); // Afslutter: DOMContentLoaded
