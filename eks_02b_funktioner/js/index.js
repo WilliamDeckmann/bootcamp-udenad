9262ce910cf29a912dcc4e22020baaf0b117266a
WilliamDeckmann <u773560@rts-365.dk>
2021-08-19 11:26:36 +0200

    Add assignment 02b

--- eks_02b_funktioner/js/index.js
+++ eks_02b_funktioner/js/index.js
@@ -3,4 +3,15 @@ document.addEventListener("DOMContentLoaded", () => {
 
 	// skriv din kode her...
 
+	// Function
+	function hils(navn) {
+		
+		console.log(`Hej med dig, ${navn}!`);
+	};
+	
+	// Initiators
+	hils("James");
+	hils("George");
+	hils("Mike");
+
 }); // Afslutter: DOMContentLoaded
