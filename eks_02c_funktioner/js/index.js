3ffdcd5b7a8e48e5d33c660ef86db595ffc37cb1
WilliamDeckmann <u773560@rts-365.dk>
2021-08-19 12:43:44 +0200

    Add assignment 02c

--- eks_02c_funktioner/js/index.js
+++ eks_02c_funktioner/js/index.js
@@ -3,4 +3,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
 	// skriv din kode her...
 
+	// Function
+	function hils(navn) {
+		
+		return(`Hej med dig, ${navn}!`);
+	};
+
+	// Variables
+	let Hils_Frank = hils("Frank")
+	
+	// Initiators
+	console.log(Hils_Frank)
+
 }); // Afslutter: DOMContentLoaded
